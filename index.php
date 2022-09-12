bd9984e348b54f5cce9d0761ffbf890b20a1de0c
sukhdeep sandhu <sandhusukhdeep2@gmail.com>
2022-09-12 17:45:22 +1000

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home Page</title>
-</head>
-<style type="text/css">
-	background-color:red;
-</style>
+</head> 
 <body>
 <h1>hello world to you</h1>
 </body>
